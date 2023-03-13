51ce4d0fff5b7eb2d5dc7a612e95f75673cd143e
周哲 <1708834102@qq.com>
2023-03-13 10:12:21 +0800

    update

--- contracts/GMXSwapper.sol
+++ contracts/GMXSwapper.sol
@@ -9,11 +9,15 @@ contract GMXSwapper is Ownable {
 
     address public constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1; // wrapped ETH
     address public constant GMX = 0xfc5A1A6EB076a2C7aD06eD22C90d7E710E35ad0a;
-    address public constant ROUTER_1INCH = 0x1111111254EEB25477B68fb85Ed929f73A960582;
+    address public constant ROUTER_UNISWAP = 0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5;
     address public constant EXECUTOR = 0x64768A3a2453F1E8DE9e43e92D65Fc36E4c9872d;
     address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
-    function swap() public {
+    constructor() {
+
+    }
+
+    function executeSwap(address _recevier, address _tokenIn, address _tokenOut, uint256 _amountIn) public {
       
     }
 
